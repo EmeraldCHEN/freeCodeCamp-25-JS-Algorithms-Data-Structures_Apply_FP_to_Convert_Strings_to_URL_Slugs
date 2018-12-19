94c881f1b1ffc6908d3592d9c8628e9809238987
EmeraldNZ <ahyuancy@gmail.com>
2018-12-20 09:48:14 +1300

    Update .js

--- .js
+++ .js
@@ -1,12 +1,11 @@
 
-var globalTitle = "Winter Is Coming";
-var globalTitle666 = " Winter Is        Coming";
+var globalTitle = "    Winter     Is Coming";
 
 function urlSlug(title) {
-  let arr = [];
+    let arr = [];
     let regex1 = /[A-Z]/;
     let regex2 = /\s/;
-    for(let i = 0; i < title.length; i++){
+    for(let i = 0; (i-1) < title.length; i++){
         arr.push(title[i]);
         if(regex1.test(arr[i]) === true && regex2.test(arr[i-1]) === true){
             arr.splice((i-1),1,'-');
@@ -22,13 +21,11 @@ function urlSlug(title) {
     slug1 = slug1.join('').toLowerCase();
     slug2 = slug1.slice(1);
 
-    return (slug1[0] == "-" ?  slug2:  slug1);
-  
+    return (slug1[0] == "-" ?  slug2:  slug1);  
 }
 
+var winterComing = urlSlug(globalTitle); // Returns "winter-is-coming"
 
-var winterComing = urlSlug(globalTitle); // Should be "winter-is-coming"
-var winterComing = urlSlug(globalTitle666);
 
 
 
