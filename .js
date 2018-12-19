f69f9770c8045b54d5c2632de461fc258daf11e7
EmeraldNZ <ahyuancy@gmail.com>
2018-12-20 09:40:28 +1300

    Create .js

---
+++ .js
@@ -0,0 +1,38 @@
+
+var globalTitle = "Winter Is Coming";
+var globalTitle666 = " Winter Is        Coming";
+
+function urlSlug(title) {
+  let arr = [];
+    let regex1 = /[A-Z]/;
+    let regex2 = /\s/;
+    for(let i = 0; i < title.length; i++){
+        arr.push(title[i]);
+        if(regex1.test(arr[i]) === true && regex2.test(arr[i-1]) === true){
+            arr.splice((i-1),1,'-');
+        }
+    } 
+    let slug1 = [];
+    let slug2 = [];
+    for(let j = 0; j < arr.length; j++){
+       if(arr[j] != " "){
+          slug1.push(arr[j]);
+       }
+    }
+    slug1 = slug1.join('').toLowerCase();
+    slug2 = slug1.slice(1);
+
+    return (slug1[0] == "-" ?  slug2:  slug1);
+  
+}
+
+
+var winterComing = urlSlug(globalTitle); // Should be "winter-is-coming"
+var winterComing = urlSlug(globalTitle666);
+
+
+
+
+
+// Retrieved from
+// https://guide.freecodecamp.org/certifications/javascript-algorithms-and-data-structures/functional-programming/apply-functional-programming-to-convert-strings-to-url-slugs/
