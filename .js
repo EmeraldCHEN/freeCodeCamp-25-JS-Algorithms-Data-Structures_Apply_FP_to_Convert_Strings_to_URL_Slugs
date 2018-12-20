15da11a5ccfa1363eccfa699952cbf6595c25122
EmeraldNZ <ahyuancy@gmail.com>
2018-12-20 14:30:22 +1300

    Update .js

--- .js
+++ .js
@@ -26,8 +26,28 @@ function urlSlug(title) {
 
 var winterComing = urlSlug(globalTitle); // Returns "winter-is-coming"
 
-//  \w is a shortcut to match alphanumerics [A-Za-z0-9_] including the underscore character _
-// \ W is the same as [^A-Za-z0-9_] to match everything but letters & numbers
 
-// Retrieved from
-// https://guide.freecodecamp.org/certifications/javascript-algorithms-and-data-structures/functional-programming/apply-functional-programming-to-convert-strings-to-url-slugs/
+/****** But the code above cannot remove the "_" if any  ********************************/
+
+
+
+
+function urlSlugNoUnderscore(title) {
+ 
+     let slug = title.split(/[\W_]+/g);
+
+     // if  let slug = title.split(/\W/);  -->  slug is an array  has no punctuation but the underscores still remain
+
+     return slug.filter(element => element !== '').join('-').toLowerCase(); 
+}
+var title = "_Winter Is_ Coming";
+
+urlSlugNoUnderscore(title);  // Returns "winter-is-coming"
+
+
+/*  \w is a shortcut to match alphanumerics [A-Za-z0-9_] including the underscore character _
+    \W is the same as [^A-Za-z0-9_] to match everything but letters & numbers
+
+ Retrieved from
+ https://guide.freecodecamp.org/certifications/javascript-algorithms-and-data-structures/functional-programming/apply-functional-programming-to-convert-strings-to-url-slugs/
+*/
